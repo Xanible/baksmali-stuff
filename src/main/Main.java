013f8497a9c5dfda59b161736f935944e9e612b0
Xanible <cbp1222@jagmail.southalabama.edu>
2018-04-17 15:32:54 -0500

    methods
    Methods are finished. Need to be looped to work properl

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,23 +3,31 @@ package main;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//moveFromSubDirs("C:\\Users\\colbyadmin\\Desktop\\out");
-		
-		//combineToSingleFile("C:\\Users\\colbyadmin\\Desktop\\out", "C:\\Users\\colbyadmin\\Desktop\\output");
-		
-		cleanDisassembly("C:\\users\\colbyadmin\\Desktop\\output");
+		moveFromSubDirs("C:\\Users\\colby\\Desktop\\out");
+
+		combineToSingleFile("C:\\Users\\colby\\Desktop\\out", "C:\\Users\\colby\\Desktop\\output");
+
+		cleanDisassembly("C:\\users\\colby\\Desktop\\output","C:\\users\\colby\\Desktop\\Cleaned\\a");
+
+		pairListCreator("C:/Users/colby/desktop/cleaned");
 	}
 
 	/**
@@ -56,7 +64,7 @@ public class Main {
 		}
 	}
 
-	
+
 	/**
 	 * This method reads in all the files in a folder and combines them into one file
 	 * 
@@ -68,7 +76,7 @@ public class Main {
 	public static void combineToSingleFile(String s, String output) {
 		File source = new File(s);
 		File[] files = source.listFiles();
-		
+
 		BufferedWriter outputFile = null;
 		try {
 			outputFile = new BufferedWriter(new FileWriter(output + File.separator + source.getName() + ".txt"));
@@ -76,7 +84,7 @@ public class Main {
 			System.out.println("Error creating output file");
 			e.printStackTrace();
 		}
-		
+
 		for(File f: files) {
 			try {
 				BufferedReader input = new BufferedReader(new FileReader(f));
@@ -92,7 +100,7 @@ public class Main {
 				e.printStackTrace();
 			}
 		}
-		
+
 		try {
 			outputFile.close();
 		} catch (IOException e) {
@@ -102,27 +110,164 @@ public class Main {
 	}
 
 
-	public static void cleanDisassembly(String s) {
+	public static void cleanDisassembly(String s, String d) {
 		File source = new File(s);
 		File[] fileList = source.listFiles();
-		
+
+		//Read in the dictionary
+		BufferedReader br = null;
+		BufferedWriter notInDiction = null;
+		List<String> dictionary = new ArrayList<String>();
+		List<String> NID = new ArrayList<String>();
+		try {
+			br = new BufferedReader(new FileReader("C:/Users/colby/Desktop/school/ADCT/Dalvik_Dictionary_Sorted.txt"));
+			String line = br.readLine();
+			while(line != null) {
+				dictionary.add(line);
+				line = br.readLine();
+			}
+			notInDiction = new BufferedWriter(new FileWriter("C:/Users/colby/Desktop/school/ADCT/NotInDictionary.txt"));
+			br.close();
+		} catch (IOException e) {
+			System.out.println("Error opening dictionary file");
+			e.printStackTrace();
+		}
+
 		for(File f: fileList) {
+			BufferedWriter output = null;
+			try {
+				output = new BufferedWriter(new FileWriter(d + File.separator + f.getName()));
+			} catch (IOException e) {
+				System.out.println("Error opening output file for cleaned disassembly");
+				e.printStackTrace();
+			}
 			List<String> opcodes = new ArrayList<String>();
 			readInDisassembly(f, opcodes);
+			for(String o: opcodes) {
+				if(dictionary.contains(o)) {
+					try {
+						output.write(o);
+						output.write(" ");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else if (!NID.contains(o)) {
+					NID.add(o);
+				}
+			}
+			try {
+				output.close();
+			} catch (IOException e) {
+				System.out.println("Error closing output file");
+				e.printStackTrace();
+			}
+		}
+		for(String n: NID) {
+			try {
+				notInDiction.write(n);
+				notInDiction.newLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			notInDiction.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static void readInDisassembly(File f, List<String> opcodes) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(f));
 			String line = br.readLine();
 			while(line != null) {
-				String[] words = line.split(" ");
-				System.out.println(words[0]);
+				String[] words = line.trim().split(" ");
+				if(words[0].equals(".end")) {
+					opcodes.add(words[0] + "-" + words[1]);
+				} else {
+					opcodes.add(words[0]);
+				}
 				line = br.readLine();
 			}
+			br.close();
 		} catch (Exception e) {
-			
+			System.out.println("Error reading in Disassembly");
+		}
+	}
+
+	public static void pairListCreator(String s) {
+		int maxStepSize = 10;
+
+		for(int stepSize = 1; stepSize <= maxStepSize; stepSize++) {
+			// Access the directory containing the directories that contain the files
+			File sourceDir = new File(s);
+
+			// Create an array of the directories
+			File[] dirs = sourceDir.listFiles();
+
+			// Create hashmap and iterator
+			HashMap<String, Integer> listOfPairs = new HashMap<String, Integer>();
+			int count = 0;
+
+			// Loop through directories
+			for(File dir: dirs) {
+				// Create list of files in current directory
+				File[] dirFiles = dir.listFiles();
+
+				// Loop through files
+				for(File f: dirFiles) {
+					count++;
+					try {
+						// Convert file to array list
+						List<String> theFile = Files.readAllLines(f.toPath(), Charset.defaultCharset() );
+						String[] words = theFile.get(0).split(" ");
+						List<String> wordsList = new ArrayList<String>(Arrays.asList(words));
+
+						// Go through the file
+						for(int i = 0;i < wordsList.size() - stepSize;i++) {
+							if(!wordsList.get(i).equals(".end-method")) {	
+								List<String> l = new ArrayList<String>();
+								for(int j = 1; j < stepSize; j++) {
+									l.add(wordsList.get(i + j));
+								}
+								if(!l.contains(".end-method")) {
+									String p = wordsList.get(i) + " " + wordsList.get(i + stepSize);
+									// Add pairing to hashmap
+									listOfPairs.put(p, count);
+								}
+							}
+						}
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			// Create sets from hashmaps
+			Set<String> uniqueValues = new HashSet<String>(listOfPairs.keySet()); 
+			// Output number of unique values in the set
+			System.out.println("The number of unique words for step size " + stepSize + ": "+uniqueValues.size());
+
+			// Output pairs to file
+			BufferedWriter bw;
+			try {
+				bw = new BufferedWriter(new FileWriter("C:\\Users\\colby\\Desktop\\PL\\Pair" + stepSize + ".txt"));
+				for(String u: uniqueValues) {
+					bw.write(u);
+					bw.newLine();
+				}
+				bw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
