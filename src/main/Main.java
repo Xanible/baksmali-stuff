9c80f9c3ca7f428c1979da5739c3288a48db6001
Xanible <cbp1222@jagmail.southalabama.edu>
2018-04-17 00:02:28 -0500

    next

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,20 +3,23 @@ package main;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		moveFromSubDirs("C:\\Users\\colby\\Desktop\\out");
+		//moveFromSubDirs("C:\\Users\\colbyadmin\\Desktop\\out");
 		
-		combineToSingleFile("C:\\Users\\colby\\Desktop\\out", "C:\\Users\\colby\\Desktop\\output\\Malware");
+		//combineToSingleFile("C:\\Users\\colbyadmin\\Desktop\\out", "C:\\Users\\colbyadmin\\Desktop\\output");
+		
+		cleanDisassembly("C:\\users\\colbyadmin\\Desktop\\output");
 	}
 
 	/**
@@ -53,13 +56,22 @@ public class Main {
 		}
 	}
 
+	
+	/**
+	 * This method reads in all the files in a folder and combines them into one file
+	 * 
+	 * Finished
+	 * 
+	 * @param s
+	 * @param output
+	 */
 	public static void combineToSingleFile(String s, String output) {
 		File source = new File(s);
 		File[] files = source.listFiles();
 		
 		BufferedWriter outputFile = null;
 		try {
-			outputFile = new BufferedWriter(new FileWriter(output + File.separator + source.getName()));
+			outputFile = new BufferedWriter(new FileWriter(output + File.separator + source.getName() + ".txt"));
 		} catch (IOException e) {
 			System.out.println("Error creating output file");
 			e.printStackTrace();
@@ -68,8 +80,14 @@ public class Main {
 		for(File f: files) {
 			try {
 				BufferedReader input = new BufferedReader(new FileReader(f));
-				input
-			} catch (FileNotFoundException e) {
+				String line = input.readLine();
+				while(line != null) {
+					outputFile.write(line);
+					line = input.readLine();
+					outputFile.newLine();
+				}
+				input.close();
+			} catch (IOException e) {
 				System.out.println("Error opening one of the source files");
 				e.printStackTrace();
 			}
@@ -80,6 +98,31 @@ public class Main {
 		} catch (IOException e) {
 			System.out.println("Error closing output file");
 			e.printStackTrace();
+		}
+	}
+
+
+	public static void cleanDisassembly(String s) {
+		File source = new File(s);
+		File[] fileList = source.listFiles();
+		
+		for(File f: fileList) {
+			List<String> opcodes = new ArrayList<String>();
+			readInDisassembly(f, opcodes);
+		}
+	}
+	
+	public static void readInDisassembly(File f, List<String> opcodes) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String line = br.readLine();
+			while(line != null) {
+				String[] words = line.split(" ");
+				System.out.println(words[0]);
+				line = br.readLine();
+			}
+		} catch (Exception e) {
+			
 		}
 	}
 }
